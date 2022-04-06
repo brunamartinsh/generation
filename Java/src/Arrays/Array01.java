a2f811d82d448ba34c0aa7f734a1a8ef6fa4263d
Bruna Maria <brunamartinsdeh@gmail.com>
2022-04-05 22:58:56 -0300

    Arrays

---
+++ Java/src/Arrays/Array01.java
@@ -0,0 +1,24 @@
+package Arrays;
+
+public class Array01 {
+	
+	public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			{
+					// TODO Auto-generated method stub
+					int[] A = {1,0,5,-2,-5,7};
+					int soma,i;
+					
+					soma= A[0]+A[1]+A[5];
+					A[3]=100;
+					
+					for(i=0;i<6;i++) {
+						System.out.println("\nVetor A: "+A[i]);
+					}
+					System.out.println("\nResultado da soma: "+soma);
+				}
+
+			}
+
+		}
+
