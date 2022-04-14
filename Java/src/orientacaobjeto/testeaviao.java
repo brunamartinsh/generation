7e5e67ed10bdfa347180cd049074193a958b0f58
Bruna Maria <brunamartinsdeh@gmail.com>
2022-04-14 11:17:36 -0300

    Ficha de exercícios de orientação ao objeto

---
+++ Java/src/orientacaobjeto/testeaviao.java
@@ -0,0 +1,30 @@
+package orientacaobjeto;
+
+	import java.util.Scanner;
+
+	public class testeaviao {
+
+		public static void main(String[] args) {
+			Scanner sc = new Scanner(System.in);
+			String modelo, fabricante, companhia;
+			int qtPassageiros, preco;
+			
+			System.out.println("digite o modelo do avi�o: ");
+			modelo = sc.next();
+			System.out.println("digite o fabricante do avi�o: ");
+			fabricante = sc.next();
+			System.out.println("digite a companhia do avi�o: ");
+			companhia = sc.next();
+			System.out.println("digite a quantidade de passageiros do avi�o: ");
+			qtPassageiros = sc.nextInt();
+			System.out.println("digite o pre�o do avi�o: ");
+			preco = sc.nextInt();
+			
+			aviao aviao = new aviao(modelo, fabricante, companhia, qtPassageiros, preco);
+			System.out.println(aviao.toString());
+			
+			sc.close();
+
+		}
+
+	}
